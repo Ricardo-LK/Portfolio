1aa67dfee641f67415a3943f6e8ae13a47db6b13
Ricardo-LK <92482482+Ricardo-LK@users.noreply.github.com>
2024-06-13 17:01:34 -0300

    Initial commit

---
+++ src/Components/projects/projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './projects.css'
+
+function Products () {
+    return (
+        <div>
+            <h2>a</h2>
+            <h2>b</h2>
+            <h2>c</h2>
+            <h2>d</h2>
+        </div>
+    )
+}
+
+export default Products
---
+++ src/Pages/portfolio.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Components/header_bar/header.jsx'
+import Projects from '../Components/projects/projects.jsx'
+
+function Portfolio() {
+  return (
+    <div>
+        <Header/>
+        <Projects/>
+    </div>
+  )
+}
+
+export default Portfolio
